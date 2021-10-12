d2434cfdace19adca4d3443bf84c6e83087b1c34
Andrey <karl911291@mail.ru>
2021-10-12 16:47:35 +0300

    Add Duplicates project

---
+++ MyTasks/Duplicates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Duplicates
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] array = { 2, 2, 3, 4, 5, 6, 7, 7, 7, 8, 10};
+      var duplicateValue = new Dictionary<int, int>();
+
+      foreach (var i in array)
+      {
+        if (duplicateValue.ContainsKey(i))
+          duplicateValue[i]++;
+        else
+          duplicateValue[i] = 1;
+      }
+
+      foreach (var i in duplicateValue)
+      {
+        if (i.Value > 1)
+          Console.WriteLine("Значение {0}  Кол-во {1}", i.Key, i.Value);
+      }
+      Console.ReadKey();
+    }
+  }
+}
