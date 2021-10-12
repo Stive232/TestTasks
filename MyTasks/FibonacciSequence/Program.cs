fe0d3debb095c871d011ac6810ea4646535dcb64
Andrey <karl911291@mail.ru>
2021-10-12 22:46:32 +0300

    Add FibonacciSequence project

---
+++ MyTasks/FibonacciSequence/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciSequence
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Введите кол-во чисел Фибоначчи, которых нужно вывести на экран");
+      int quantity = int.Parse(Console.ReadLine());
+      
+      Console.WriteLine("За каким целым числом вывести последовательность?");
+      int startNumber = int.Parse(Console.ReadLine());
+
+      int x = 1;
+      int y = 1;
+      int i = 0;
+
+      if(startNumber < 1)
+      {
+        Console.Write("1 1 ");
+        i = 2;
+      }
+        
+      for (; i < quantity; i++)
+      {
+        y = x + y;
+        x = y - x;
+        if (startNumber >= y)
+        {
+          i--;
+          continue;
+        }       
+        Console.Write($"{y} ");
+      }
+    }
+  }
+}
