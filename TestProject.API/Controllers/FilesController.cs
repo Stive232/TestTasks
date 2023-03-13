58dffce513e689238e663fceb645d4ae47ecfd5f
Andrey <karl911291@mail.ru>
2023-03-13 23:21:00 +0300

    Add FilesController

---
+++ TestProject.API/Controllers/FilesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestProject.Controllers;
+
+public class FilesController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> PostFile()
+    {
+        await Task.Delay(1000);
+        return Ok();
+    }
+}
